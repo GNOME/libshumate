35e38229638f58cea70dcbc2ffd73d777f8c0ca4
Georges Basile Stavracas Neto <georges.stavracas@gmail.com>
2021-03-02 14:49:35 -0300

    tests: Add new marker test for the new API
    Who doesn't like an unit test anyway? :)

---
+++ tests/marker.c
@@ -0,0 +1,30 @@
+#include <gtk/gtk.h>
+#include <shumate/shumate.h>
+
+static void
+test_marker_child (void)
+{
+  ShumateMarker *marker;
+  GtkWidget *button;
+
+  marker = shumate_marker_new ();
+  g_assert_null (gtk_widget_get_first_child (GTK_WIDGET (marker)));
+
+  button = gtk_button_new ();
+  shumate_marker_set_child (marker, button);
+  g_assert_true (gtk_widget_get_first_child (GTK_WIDGET (marker)) == button);
+
+  shumate_marker_set_child (marker, NULL);
+  g_assert_null (gtk_widget_get_first_child (GTK_WIDGET (marker)));
+}
+
+int
+main (int argc, char *argv[])
+{
+  g_test_init (&argc, &argv, NULL);
+  gtk_init ();
+
+  g_test_add_func ("/marker/child", test_marker_child);
+
+  return g_test_run ();
+}
