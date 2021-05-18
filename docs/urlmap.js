02aaa3411168534a3d274be114843037aca02a72
James Westman <james@flyingpimonster.net>
2021-05-17 19:42:08 -0500

    docs: Switch to gi-docgen
    - Don't build docs in flatpak. It would require a bunch of extra modules
      for gi-docgen's dependencies, and doc generation can take a little
      while anyway. Better to leave it out.
    - Remove the gtk-doc files and add gi-docgen to the build system.

---
+++ docs/urlmap.js
@@ -0,0 +1,16 @@
+// SPDX-FileCopyrightText: 2021 GNOME Foundation
+// SPDX-License-Identifier: LGPL-2.1-or-later
+
+// Copied from GTK
+
+// A map between namespaces and base URLs for their online documentation
+baseURLs = [
+    [ 'Gdk', 'https://docs.gtk.org/gdk4/' ],
+    [ 'GdkWayland', 'https://gnome.pages.gitlab.gnome.org/gtk/gdk4-wayland/' ],
+    [ 'GdkX11', 'https://gnome.pages.gitlab.gnome.org/gtk/gdk4-x11/' ],
+    [ 'Gsk', 'https://docs.gtk.org/gsk4/' ],
+    [ 'Gtk', 'https://docs.gtk.org/gtk4/' ],
+    [ 'Pango', 'https://docs.gtk.org/Pango/' ],
+    [ 'PangoCairo', 'https://gnome.pages.gitlab.gnome.org/pango/pangocairo/' ],
+]
+
