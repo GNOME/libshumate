af6591e106672fc263a2a279161a5e02ee4dac93
Chris Mayo <aklhfex@gmail.com>
2025-07-01 19:22:19 +0100

    docs: Fix links to GObject documentation
    Update dependencies link and enable class ancestors links to be mapped.

--- docs/urlmap.js
+++ docs/urlmap.js
@@ -5,6 +5,7 @@
 
 // A map between namespaces and base URLs for their online documentation
 baseURLs = [
+    [ 'GObject', 'https://docs.gtk.org/gobject/' ],
     [ 'Gdk', 'https://docs.gtk.org/gdk4/' ],
     [ 'GdkWayland', 'https://gnome.pages.gitlab.gnome.org/gtk/gdk4-wayland/' ],
     [ 'GdkX11', 'https://gnome.pages.gitlab.gnome.org/gtk/gdk4-x11/' ],
