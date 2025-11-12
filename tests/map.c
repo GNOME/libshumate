375b0d217123d5336af7412164be60af0225bce6
jochen@homeland <no-go@localhost>
2025-11-12 15:15:39 +0100

    map: Add functionality for zoom-on-double-click
    Implement the functionality for set_zoom_on_double_click.
    Also add a test case for this.

--- tests/map.c
+++ tests/map.c
@@ -4,6 +4,20 @@
 #include <shumate/shumate.h>
 
 static void
+emit_double_click (ShumateMap *map)
+{
+  g_autoptr(GListModel) controllers = gtk_widget_observe_controllers (GTK_WIDGET (map));
+  for (guint i = 0; i < g_list_model_get_n_items (controllers); i++) {
+      g_autoptr(GtkEventController) controller = g_list_model_get_item (controllers, i);
+
+      if (GTK_IS_GESTURE_CLICK (controller)) {
+          // emit a double click
+          g_signal_emit_by_name (controller, "pressed", 2, 10.0, 20.0);
+      }
+  }
+}
+
+static void
 test_map_add_layers (void)
 {
   ShumateMap *map = shumate_map_new ();
@@ -51,6 +65,29 @@ test_map_add_layers (void)
   shumate_map_remove_layer (map, layer3);
 }
 
+static void
+test_map_zoom_on_double_click_switch (void)
+{
+  ShumateMap *map = shumate_map_new ();
+  ShumateViewport *viewport = shumate_map_get_viewport (map);
+  // initial zoom is 10
+  double initial = 10;
+
+  // test: zoom on double click should NOT work
+  shumate_viewport_set_zoom_level (viewport, initial);
+  shumate_map_set_zoom_on_double_click (map, FALSE);
+  emit_double_click (map);
+  double actual = shumate_viewport_get_zoom_level (viewport);
+  g_assert_cmpfloat_with_epsilon (actual, initial, 0.0001);
+
+  // test: zoom on double click should work
+  shumate_viewport_set_zoom_level (viewport, initial);
+  shumate_map_set_zoom_on_double_click (map, TRUE);
+  emit_double_click (map);
+  actual = shumate_viewport_get_zoom_level (viewport);
+  g_assert_true (actual > initial);
+}
+
 int
 main (int argc, char *argv[])
 {
@@ -58,6 +95,7 @@ main (int argc, char *argv[])
   gtk_init ();
 
   g_test_add_func ("/map/add-layers", test_map_add_layers);
+  g_test_add_func ("/map/zoom_on_double_click_switch", test_map_zoom_on_double_click_switch);
 
   return g_test_run ();
 }
